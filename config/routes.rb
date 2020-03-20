df79e8d87a55411fb0730d0bb5820c7d494b8f21
Adebola <onedebos@gmail.com>
2020-03-20 16:23:37 +0100

    mount graphiql in production

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
  
-  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql" if Rails.env.development?
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   post "/graphql", to: "graphql#execute"
   # post 'calculate', to: 'cryptos#calculate'
 end
